8ef22c124ddc8a32c9daacd2c475920cb81ed863
Matheus keler <matheusmagalhaes642@gmail.com>
2023-05-02 15:41:30 -0300

    criando classe security para cors

---
+++ src/main/java/com/desafio/apidesafio/security/WebSecurity.java
@@ -0,0 +1,14 @@
+package com.desafio.apidesafio.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurity implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+	}
+}
