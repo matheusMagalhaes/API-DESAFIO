64e2faf1b43d1833a74800d0291425e0c6e02d74
Matheus keler <matheusmagalhaes642@gmail.com>
2023-05-17 18:21:54 -0300

    valiando cpf

--- src/main/java/com/desafio/apidesafio/serviceImpl/ColaboradorServiceImpl.java
+++ src/main/java/com/desafio/apidesafio/serviceImpl/ColaboradorServiceImpl.java
@@ -3,6 +3,8 @@ package com.desafio.apidesafio.serviceImpl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.management.RuntimeErrorException;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,10 @@ import com.desafio.apidesafio.model.dto.ColaboradorDTO;
 import com.desafio.apidesafio.repository.ColaboradorRepository;
 import com.desafio.apidesafio.service.ColaboradorService;
 
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CPFValidator;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
+import jakarta.servlet.ServletException;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -38,18 +44,32 @@ public class ColaboradorServiceImpl implements ColaboradorService {
 
 	@Override
 	public Colaborador salvarColaborador(ColaboradorDTO colaboradorDTO) {
-		try {
+
+		String cpf = colaboradorDTO.getCpf();
+		if (!this.validaCpf(cpf)) {
+			throw new RuntimeException("CPF Invalido!");
+		} else {
 			Colaborador colaborador = this.modelMapper.map(colaboradorDTO, Colaborador.class);
 			return this.colaboradorRepository.save(colaborador);
 
-		} catch (Exception e) {
-			return null;
 		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		this.colaboradorRepository.deleteById(id);
+	}
+
+	public Boolean validaCpf(String cpf) {
+
+		CPFValidator cpfValidator = new CPFValidator();
+		List<ValidationMessage> errorMsg = cpfValidator.invalidMessagesFor(cpf);
+		if (errorMsg.size() > 0) {
+			return false;
+		} else {
+			return true;
+		}
+
 	}
 
 }
